877a70b51b078d963edbfa5da53d5fe35bb1eaac
Tim Heerwagen <t.heerwagen@web.de>
2022-10-21 15:10:49 +0200

    #1 Typography erstellt

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
         },
       },
     },
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
---
+++ lib/components/ui/Typography/Heading/heading.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import React from "react";
+
+import { Heading, sizes, colors } from "./heading";
+
+export default {
+  title: "UI/Heading",
+  component: Heading,
+  argTypes: {
+    size: {
+      control: { type: "select" },
+    },
+    color: {
+      control: { type: "select" },
+    },
+  },
+} as Meta;
+
+const Container = ({ ...props }) => (
+  <div className="flex flex-col gap-5" {...props} />
+);
+
+export const Sizes = ({ ...args }) => {
+  const _sizes = Object.keys(sizes).reverse();
+
+  return (
+    <Container>
+      {_sizes.map((size: any) => (
+        <Heading size={size} key={size} {...args}>
+          {size}
+        </Heading>
+      ))}
+    </Container>
+  );
+};
+
+Sizes.parameters = {
+  controls: { exclude: "size" },
+};
+export const Colors = ({ ...args }) => {
+  const _colors = Object.keys(colors);
+
+  return (
+    <Container>
+      {_colors.map((color: any) => (
+        <Heading color={color} key={color} {...args}>
+          {color}
+        </Heading>
+      ))}
+    </Container>
+  );
+};
+
+Colors.parameters = {
+  controls: { exclude: "color" },
+  a11y: {
+    disable: true,
+  },
+};
