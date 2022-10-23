10e00d2829f91bf721b4817b7ecc1201a9f5bcbf
Tim Heerwagen <t.heerwagen@web.de>
2022-10-23 14:14:28 +0200

    feat(ui): Form hinzugefügt #16

---
+++ lib/components/ui/Form/Form.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from "react";
+
+type Props = {
+  formMethods: any;
+  onSubmit: Function;
+  className?: string;
+  children: ReactNode;
+};
+
+export const Form: FC<Props> = ({
+  formMethods,
+  onSubmit,
+  className,
+  ...props
+}) => {
+  const { handleSubmit } = formMethods;
+
+  return (
+    <form className={className} onSubmit={handleSubmit(onSubmit)} {...props} />
+  );
+};
+
+Form.defaultProps = {
+  className: "",
+};
