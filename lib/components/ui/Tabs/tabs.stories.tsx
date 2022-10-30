39d31efc34a49e0a1ca4a6866ed4cb1fa2f707e1
Tim Heerwagen <t.heerwagen@web.de>
2022-10-30 16:13:42 +0100

    feat(ui): Tabs erstellt #34

---
+++ lib/components/ui/Tabs/tabs.stories.tsx
@@ -0,0 +1,99 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import React from "react";
+
+import { TabGroup, TabList, TabPanels, Tabs } from "./tabs";
+import type { Sizes as SizesType, Colors as ColorsType } from "../../../types";
+
+export default {
+  title: "UI/Disclosures/Tabs",
+  component: Tabs,
+} as Meta;
+
+const Container = ({ ...props }) => (
+  <div className="flex flex-row gap-5" {...props} />
+);
+
+export const Default = ({ ...args }) => (
+  <Container>
+    <Tabs
+      items={[
+        { title: "Tab 1", content: "Content 1" },
+        { title: "Tab 2", content: "Content 2" },
+        { title: "Tab 3", content: "Content 3" },
+      ]}
+      {...args}
+    />
+  </Container>
+);
+
+export const Sizes = ({ ...args }) => {
+  const sizes: (keyof SizesType)[] = ["xs", "sm", "md", "lg", "xl"];
+
+  return (
+    <Container>
+      {sizes.map((size) => (
+        <Tabs
+          key={size}
+          size={size}
+          items={[
+            { title: "Tab 1", content: "Content 1" },
+            { title: "Tab 2", content: "Content 2" },
+            { title: "Tab 3", content: "Content 3" },
+          ]}
+          {...args}
+        />
+      ))}
+    </Container>
+  );
+};
+
+export const Colors = ({ ...args }) => {
+  const colors: (keyof ColorsType)[] = [
+    "primary",
+    "accent",
+    "success",
+    "warn",
+    "danger",
+  ];
+
+  return (
+    <Container>
+      {colors.map((color) => (
+        <Tabs
+          key={color}
+          color={color}
+          items={[
+            { title: "Tab 1", content: "Content 1" },
+            { title: "Tab 2", content: "Content 2" },
+            { title: "Tab 3", content: "Content 3" },
+          ]}
+          {...args}
+        />
+      ))}
+    </Container>
+  );
+};
+
+Colors.parameters = {
+  controls: { exclude: "color" },
+  a11y: {
+    disable: true,
+  },
+};
+
+export const Separate = ({ ...args }) => {
+  const items = [
+    { title: "Tab 1", content: "Content 1" },
+    { title: "Tab 2", content: "Content 2" },
+    { title: "Tab 3", content: "Content 3" },
+  ];
+
+  return (
+    <Container>
+      <TabGroup>
+        <TabList items={items} {...args} />
+        <TabPanels items={items} {...args} />
+      </TabGroup>
+    </Container>
+  );
+};
---
+++ lib/components/ui/Tabs/tabs.tsx
@@ -0,0 +1,151 @@
+import { Tab } from "@headlessui/react";
+import cn from "classnames";
+import { Fragment, memo, ReactNode } from "react";
+
+import { gaps, paddings } from "../../../styles";
+import { Colors, Sizes } from "../../../types";
+import { Button } from "../Button";
+
+export type TabGroupProps = TabListProps &
+  TabPanelsProps & {
+    className?: string;
+  };
+
+type TabListProps = {
+  items: ItemProps[];
+  listClassName?: string;
+  size?: keyof Sizes;
+  color?: keyof Colors;
+};
+
+type TabPanelsProps = {
+  items: ItemProps[];
+  panelsClassName?: string;
+  size?: keyof Sizes;
+};
+
+type ItemProps = {
+  title: string;
+  content: ReactNode;
+  disabled?: boolean;
+  className?: string;
+};
+
+export const Tabs = ({
+  items = [],
+  className,
+  size = "md",
+  color,
+  panelsClassName,
+  listClassName,
+}: TabGroupProps) => (
+  <Tab.Group as="div" className={cn("flex flex-col w-full", className)}>
+    <TabList
+      items={items}
+      listClassName={listClassName}
+      size={size}
+      color={color}
+    />
+    <TabPanels items={items} panelsClassName={panelsClassName} size={size} />
+  </Tab.Group>
+);
+
+export default memo(Tabs);
+
+export const TabGroup = ({ children, ...props }: any) => (
+  <Tab.Group {...props}>{children}</Tab.Group>
+);
+
+Tabs.defaultProps = {
+  className: undefined,
+  panelsClassName: undefined,
+  listClassName: undefined,
+  size: "md",
+  color: "accent",
+};
+
+export const TabList = ({
+  items,
+  size = "md",
+  listClassName,
+  color,
+}: TabListProps) => (
+  <Tab.List className={cn("flex flex-row", gaps[size], listClassName)}>
+    {items.map(({ title, disabled }: ItemProps, i) => (
+      <TabButton
+        key={`tab_button_${title}`}
+        title={title}
+        disabled={disabled}
+        color={color}
+        size={size}
+      />
+    ))}
+  </Tab.List>
+);
+
+TabList.defaultProps = {
+  listClassName: undefined,
+  size: "md",
+  color: "accent",
+};
+
+const TabButton = ({
+  title,
+  disabled,
+  color = "accent",
+  size = "md",
+}: Omit<ItemProps, "content"> & Pick<TabListProps, "color" | "size">) => (
+  <Tab as={Fragment}>
+    {({ selected }) => (
+      <Button
+        size={size}
+        variant={!selected ? "ghost" : "filled"}
+        disabled={disabled}
+        color={color}
+      >
+        {title}
+      </Button>
+    )}
+  </Tab>
+);
+TabButton.defaultProps = {
+  diabled: undefined,
+  className: undefined,
+};
+
+export const TabPanels = ({
+  items,
+  size = "md",
+  panelsClassName,
+}: TabPanelsProps) => (
+  <Tab.Panels className={cn(panelsClassName)}>
+    {items.map(({ content, className: panelClassName }: ItemProps, i) => (
+      <TabPanel
+        key={`tab_panel_${i}`}
+        content={content}
+        className={panelClassName}
+        size={size}
+      />
+    ))}
+  </Tab.Panels>
+);
+
+TabPanels.defaultProps = {
+  panelsClassName: undefined,
+  size: "md",
+};
+
+const TabPanel = ({
+  content,
+  className,
+  size = "md",
+}: Omit<ItemProps, "title"> & Pick<TabPanelsProps, "size">) => (
+  <Tab.Panel className={cn("flex", paddings[size], className)}>
+    {content}
+  </Tab.Panel>
+);
+
+TabPanel.defaultProps = {
+  size: "md",
+  panelClassName: undefined,
+};
