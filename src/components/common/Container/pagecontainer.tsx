5d33e9c87068bbff0e7d0183c89f5c06035b609b
Tim Heerwagen <t.heerwagen@web.de>
2022-11-09 20:30:44 +0100

    feat(common): PageContainer hinzugefügt #113

---
+++ src/components/common/Container/pagecontainer.tsx
@@ -0,0 +1,38 @@
+import cn from "classnames";
+import { HTMLAttributes, useState, useEffect, MutableRefObject } from "react";
+
+interface PageContainerProps extends HTMLAttributes<HTMLDivElement> {
+  navbarRef?: MutableRefObject<any>;
+  footerRef?: MutableRefObject<any>;
+}
+
+export const PageContainer = ({
+  navbarRef,
+  footerRef,
+  ...props
+}: PageContainerProps) => {
+  const [navbarHeight, setNavbarHeight] = useState();
+  const [footerHeight, setFooterHeight] = useState();
+  useEffect(() => {
+    navbarRef && setNavbarHeight(navbarRef?.current?.clientHeight);
+    footerRef && setFooterHeight(footerRef?.current?.clientHeight);
+  }, []);
+
+  console.log(navbarHeight, footerHeight);
+
+  return (
+    <main
+      className={cn("overflow-x-hidden flex")}
+      style={{
+        paddingTop: `${navbarHeight}px`,
+        minHeight: `calc(100vh - ${footerHeight}px)`,
+      }}
+      {...props}
+    />
+  );
+};
+
+PageContainer.defaultProps = {
+  navbarRef: undefined,
+  footerRef: undefined,
+};
