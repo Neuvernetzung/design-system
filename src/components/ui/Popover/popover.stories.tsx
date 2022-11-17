5390541ee38ba0d62679bd539beeb2c0040ab886
Tim Heerwagen <t.heerwagen@web.de>
2022-11-17 14:43:21 +0100

    feat(ui): Optionalen Fullscreen Modus für Popover und maximale Höhe hinzugefügt #163

--- src/components/ui/Popover/popover.stories.tsx
+++ src/components/ui/Popover/popover.stories.tsx
@@ -22,6 +22,7 @@ export const Default = ({ ...args }) => (
         </div>
       }
       buttonProps={{ children: "Popover öffnen" }}
+      placement="bottom-start"
       {...args}
     />
     <Popover
@@ -31,6 +32,7 @@ export const Default = ({ ...args }) => (
         </div>
       }
       buttonProps={{ children: "Popover öffnen" }}
+      placement="bottom"
       {...args}
     />
     <Popover
@@ -40,6 +42,7 @@ export const Default = ({ ...args }) => (
         </div>
       }
       buttonProps={{ children: "Popover öffnen" }}
+      placement="bottom-end"
       {...args}
     />
   </Container>
@@ -63,7 +66,6 @@ export const OnHover = ({ ...args }) => (
 export const PanelClassName = ({ ...args }) => (
   <Container>
     <Popover
-      trigger="hover"
       content={
         <div>
           <Text>Dies ist ein Popover</Text>
@@ -71,7 +73,91 @@ export const PanelClassName = ({ ...args }) => (
       }
       placement="bottom-start"
       panelClassName="bg-red-500 w-64"
-      buttonProps={{ children: "Hier hovern" }}
+      buttonProps={{ children: "Panel Classname" }}
+      {...args}
+    />
+  </Container>
+);
+
+export const LargeContent = ({ ...args }) => (
+  <Container>
+    <Popover
+      content={
+        <div>
+          <Text>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in
+            vulputate velit esse molestie consequat, vel illum dolore eu feugiat
+            nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat. Duis autem vel eum iriure dolor in
+            hendrerit in vulputate velit esse molestie consequat, vel illum
+            dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+            odio dignissim qui blandit praesent luptatum zzril delenit augue
+            duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta
+            nobis eleifend option congue nihil imperdiet doming id quod mazim
+            placerat facer
+          </Text>
+        </div>
+      }
+      placement="bottom-start"
+      buttonProps={{ children: "Viel Content" }}
+      {...args}
+    />
+    <Popover
+      content={
+        <div>
+          <Text>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in
+            vulputate velit esse molestie consequat, vel illum dolore eu feugiat
+            nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing
+            elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
+            ex ea commodo consequat. Duis autem vel eum iriure dolor in
+            hendrerit in vulputate velit esse molestie consequat, vel illum
+            dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+            odio dignissim qui blandit praesent luptatum zzril delenit augue
+            duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta
+            nobis eleifend option congue nihil imperdiet doming id quod mazim
+            placerat facer
+          </Text>
+        </div>
+      }
+      placement="bottom-end"
+      fullScreenOnMobile
+      buttonProps={{ children: "Viel Content Mobile Fullscreen" }}
       {...args}
     />
   </Container>
