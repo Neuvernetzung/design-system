8afe53c08c942eed1505609826960f1d592c0a7f
Tim Heerwagen <t.heerwagen@web.de>
2022-12-01 14:22:59 +0100

    fix(ui): forwardRef zu Liste hinzugefügt um RichText Error zu fixen #237

--- src/components/ui/List/listItem.tsx
+++ src/components/ui/List/listItem.tsx
@@ -1,15 +1,22 @@
 import cn from "classnames";
-import { LiHTMLAttributes, memo } from "react";
+import { LiHTMLAttributes, memo, forwardRef, ForwardedRef } from "react";
 
 import { textColors } from "../../../styles";
 
 export interface ListItemProps extends LiHTMLAttributes<HTMLLIElement> {}
 
-export const ListItem = ({ className, ...props }: ListItemProps) => (
-  <li
-    className={cn(textColors.accent, "list-item indent-5", className)}
-    {...props}
-  />
+export const ListItem = forwardRef(
+  (
+    { className, ...props }: ListItemProps,
+    ref: ForwardedRef<HTMLLIElement>
+  ) => (
+    <li
+      ref={ref}
+      className={cn(textColors.accent, "list-item indent-5", className)}
+      {...props}
+    />
+  )
 );
+ListItem.displayName = "ListItem";
 
 export default memo(ListItem);
