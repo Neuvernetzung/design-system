5f3e8c463e0d08cf38daa1a65f3dcb0891b57b14
Tim Heerwagen <t.heerwagen@web.de>
2023-01-19 16:36:32 +0100

    fix(ui): Tooltip anzeige verbessern und Tooltip Offsets prüfen #381

--- src/styles/popper/offset.ts
+++ src/styles/popper/offset.ts
@@ -1,6 +1,3 @@
 import { OffsetsFunction } from "@popperjs/core/lib/modifiers/offset";
 
-export const popperOffset: OffsetsFunction = ({ placement }) =>
-  placement.startsWith("bottom") || placement.startsWith("top")
-    ? [0, 10]
-    : [10, 0];
+export const popperOffset: OffsetsFunction = () => [0, 10];
