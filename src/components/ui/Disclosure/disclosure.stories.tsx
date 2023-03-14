b564c488863fcb9920f8b21d0df2ca120fccbcfd
Tim Heerwagen <t.heerwagen@web.de>
2023-03-14 15:11:27 +0100

    fix(ui): Disclosure Animation optimieren #497

--- src/components/ui/Disclosure/disclosure.stories.tsx
+++ src/components/ui/Disclosure/disclosure.stories.tsx
@@ -1,5 +1,4 @@
-import { ArrowDownIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
-import { Meta, Story } from "@storybook/react/types-6-0";
+import { Meta } from "@storybook/react/types-6-0";
 import React from "react";
 
 import { Disclosure, sizes } from "./disclosure";
