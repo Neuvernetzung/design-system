af152b13a31af305280480a287f0075f7dc00b1f
Tim Heerwagen <t.heerwagen@web.de>
2023-03-29 19:23:16 +0200

    feat(deps): Jede Komponente einzelnd exportieren

--- src/utils/internal/index.ts
+++ src/utils/internal/index.ts
@@ -2,5 +2,6 @@ export * from "./blendColor";
 export * from "./createCSSSelector";
 export * from "./keyboardEvent";
 export * from "./mergeRefs";
+export * from "./regex";
 export * from "./typedMemo";
 export * from "./updateQuery";
