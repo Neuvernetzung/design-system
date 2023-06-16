f5865d891ffdedd7598c767a4e2a37fd7ae7a760
Tim Heerwagen <t.heerwagen@web.de>
2023-06-16 20:29:32 +0200

    ThemeSwitch und ThemeSwitchMenu Komponenten trennen #720

--- src/components/common/ThemeSwitch/themeSwitch.tsx
+++ src/components/common/ThemeSwitch/themeSwitch.tsx
@@ -4,65 +4,61 @@ import { ComputerDesktopIcon, MoonIcon, SunIcon } from "../../../theme/icons";
 import { IconButton, Menu } from "../../ui";
 
 type ThemeSwitchProps = {
-  variant?: "button" | "menu";
   textColor?: string;
 };
 
-export const ThemeSwitch = ({
-  variant = "button",
-  textColor,
-}: ThemeSwitchProps) => {
-  const { theme, setTheme, resolvedTheme } = useTheme();
+export const ThemeSwitch = ({ textColor }: ThemeSwitchProps) => {
+  const { setTheme, resolvedTheme } = useTheme();
 
   const handleSwitchTheme = () => {
     if (resolvedTheme === "light") setTheme("dark");
     if (resolvedTheme === "dark") setTheme("light");
   };
 
-  if (variant === "button")
-    return (
-      <IconButton
-        onClick={handleSwitchTheme}
-        variant="ghost"
-        ariaLabel="theme-switch"
-        className={textColor}
-        icon={resolvedTheme === "light" ? SunIcon : MoonIcon}
-      />
-    );
+  return (
+    <IconButton
+      onClick={handleSwitchTheme}
+      variant="ghost"
+      ariaLabel="theme-switch"
+      className={textColor}
+      icon={resolvedTheme === "light" ? SunIcon : MoonIcon}
+    />
+  );
+};
 
-  if (variant === "menu")
-    return (
-      <Menu
-        dropdownClassName="min-w-[8rem] w-16 bg-red-500"
-        buttonType="icon"
-        buttonProps={{
-          ariaLabel: "theme-switch",
-          icon: resolvedTheme === "light" ? SunIcon : MoonIcon,
-          variant: "ghost",
-          className: textColor,
-        }}
-        items={[
-          {
-            icon: ComputerDesktopIcon,
-            children: "System",
-            onClick: () => setTheme("system"),
-            color: theme === "system" ? "primary" : "accent",
-          },
-          {
-            icon: SunIcon,
-            children: "Light",
-            onClick: () => setTheme("light"),
-            color: theme === "light" ? "primary" : "accent",
-          },
-          {
-            icon: MoonIcon,
-            children: "Dark",
-            onClick: () => setTheme("dark"),
-            color: theme === "dark" ? "primary" : "accent",
-          },
-        ]}
-      />
-    );
+export const ThemeSwitchMenu = ({ textColor }: ThemeSwitchProps) => {
+  const { theme, setTheme, resolvedTheme } = useTheme();
 
-  return null;
+  return (
+    <Menu
+      dropdownClassName="min-w-[8rem] w-16 bg-red-500"
+      buttonType="icon"
+      buttonProps={{
+        ariaLabel: "theme-switch",
+        icon: resolvedTheme === "light" ? SunIcon : MoonIcon,
+        variant: "ghost",
+        className: textColor,
+      }}
+      items={[
+        {
+          icon: ComputerDesktopIcon,
+          children: "System",
+          onClick: () => setTheme("system"),
+          color: theme === "system" ? "primary" : "accent",
+        },
+        {
+          icon: SunIcon,
+          children: "Light",
+          onClick: () => setTheme("light"),
+          color: theme === "light" ? "primary" : "accent",
+        },
+        {
+          icon: MoonIcon,
+          children: "Dark",
+          onClick: () => setTheme("dark"),
+          color: theme === "dark" ? "primary" : "accent",
+        },
+      ]}
+    />
+  );
 };
