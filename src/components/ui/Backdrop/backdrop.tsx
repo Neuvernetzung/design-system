c332c62661139e61128b7676ef17ddf747a36e52
Tim Heerwagen <t.heerwagen@web.de>
2023-08-20 19:20:46 +0200

    Navbar Z-Index prüfen. #887

--- src/components/ui/Backdrop/backdrop.tsx
+++ src/components/ui/Backdrop/backdrop.tsx
@@ -1,6 +1,7 @@
 import cn from "classnames";
 import { LazyMotion, domAnimation, m } from "framer-motion";
 import { typedMemo } from "../../../utils/internal";
+import { zIndexes } from "../../../styles";
 
 export const Backdrop = () => (
   <LazyMotion features={domAnimation}>
@@ -8,7 +9,10 @@ export const Backdrop = () => (
       initial={{ opacity: 0 }}
       animate={{ opacity: 100 }}
       transition={{ duration: 0.2 }}
-      className={cn("fixed inset-0 bg-opacity-25 bg-black backdrop-blur-sm")}
+      className={cn(
+        "fixed inset-0 bg-opacity-25 bg-black backdrop-blur-sm",
+        zIndexes.modal
+      )}
     />
   </LazyMotion>
 );
