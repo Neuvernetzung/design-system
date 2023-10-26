3eaf7f86a8cee020fe04b75eae57ecc38056bb45
Tim Heerwagen <t.heerwagen@web.de>
2023-10-26 14:47:59 +0200

    DefaultValue und initialView zu useCalendar hinzufügen #1080

--- src/components/ui/Calendar/hooks/useCalendar.tsx
+++ src/components/ui/Calendar/hooks/useCalendar.tsx
@@ -3,7 +3,16 @@ import { Day, useLilius } from "use-lilius";
 export type UseCalendar = typeof useCalendar;
 export type UseCalendarProps = ReturnType<UseCalendar>;
 
-type UseCalendarOwnProps = { cols?: number };
+export type UseCalendarOwnProps = {
+  cols?: number;
+  defaultValue?: Date[];
+  initialView?: Date;
+};
 
 export const useCalendar = (props?: UseCalendarOwnProps) =>
-  useLilius({ weekStartsOn: Day.MONDAY, numberOfMonths: props?.cols });
+  useLilius({
+    weekStartsOn: Day.MONDAY,
+    numberOfMonths: props?.cols,
+    selected: props?.defaultValue,
+    viewing: props?.initialView,
+  });
