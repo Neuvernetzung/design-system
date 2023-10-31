1a13883cf2d398fb7e2eb772d902a4f2c036f995
Tim Heerwagen <t.heerwagen@web.de>
2023-10-31 17:18:43 +0100

    CarouselModal exposen #1099

---
+++ .changeset/good-beans-look.md
@@ -0,0 +1,5 @@
+---
+"@neuvernetzung/design-system": patch
+---
+
+CarouselModal exposen #1099
--- src/components/ui/Carousel/index.ts
+++ src/components/ui/Carousel/index.ts
@@ -1,2 +1,8 @@
 export type { CarouselProps } from "./carousel";
 export { default as Carousel } from "./carousel";
+export { CarouselModal, useCarousel } from "./carouselModal";
+export type {
+  CarouselModalController,
+  CarouselModalProps,
+  UseCarouselProps,
+} from "./carouselModal";
