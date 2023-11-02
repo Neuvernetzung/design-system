08226d65119969683923445f41a57df4bcb1da2f
Tim Heerwagen <t.heerwagen@web.de>
2023-11-02 09:11:01 +0100

    Footer - Gestaltungsmöglichnkeiten hinzufügen #1102

---
+++ .changeset/tender-yaks-joke.md
@@ -0,0 +1,5 @@
+---
+"@neuvernetzung/design-system": patch
+---
+
+Footer - Gestaltungsmöglichnkeiten hinzufügen #1102
--- src/components/common/Footer/footer.tsx
+++ src/components/common/Footer/footer.tsx
@@ -1,11 +1,20 @@
 import cn from "classnames";
 import { forwardRef, ReactElement, ReactNode } from "react";
 
-import { gaps, paddingsX, paddingsY, pagePaddings } from "../../../styles";
+import {
+  extendedBgColors,
+  extendedBorders,
+  gaps,
+  paddingsX,
+  paddingsYLarge,
+  pagePaddings,
+} from "../../../styles";
 import { useThemeStateValue } from "../../../theme";
-import type { Size } from "../../../types";
+import type { ExtendedColor, Size, SvgType } from "../../../types";
+import { smallerSize } from "../../../utils";
 import { Link } from "../../ui/Link";
 import { Text } from "../../ui/Typography/Text";
+import { Icon } from "../../ui/Icon";
 
 type FooterProps = {
   logo?: ReactNode;
@@ -13,9 +22,11 @@ type FooterProps = {
   copyright?: string;
   className?: string;
   size?: Size;
+  color?: ExtendedColor;
 };
 
 type LinkGroupProps = {
+  icon?: SvgType;
   label?: string | ReactElement;
   links: LinkProps[];
 };
@@ -25,43 +36,75 @@ type LinkProps = {
   href: string;
 };
 
+const colsClassName: Record<number, string> = {
+  0: "",
+  1: "grid-cols-1",
+  2: "grid-cols-1 sm:grid-cols-2",
+  3: "grid-cols-1 sm:grid-cols-2 md:grid-cols-3",
+  4: "grid-cols-1 sm:grid-cols-2 md:grid-cols-4",
+};
+
 export const Footer = forwardRef<HTMLDivElement, FooterProps>(
-  ({ logo, cols, copyright, className, size = "md" }, ref) => {
+  (
+    { logo, cols = [], copyright, className, size = "md", color = "white" },
+    ref
+  ) => {
     const pagePadding = useThemeStateValue((state) => state.pagePadding);
+    const adjustedColors = useThemeStateValue(
+      (state) => state.adjustedTextColorState
+    );
 
     return (
       <footer
         ref={ref}
         className={cn(
-          "bg-white dark:bg-black flex flex-col",
+          "flex flex-col",
           gaps[size],
+          extendedBgColors[color],
           className
         )}
       >
         <div
-          className={cn(
-            "w-full max-w-6xl mx-auto",
-            paddingsY[size],
-            pagePaddings[pagePadding]
-          )}
+          className={cn("w-full max-w-6xl mx-auto", pagePaddings[pagePadding])}
         >
           <div
             className={cn(
-              "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4",
-              gaps[size]
+              "grid",
+              colsClassName[cols?.length || 0],
+              gaps[size],
+              paddingsYLarge[size]
             )}
           >
             {cols &&
-              cols?.map(({ label, links }, i) => (
+              cols?.map(({ icon, label, links }, i) => (
                 <ul
                   className="items-start flex flex-col"
                   key={`footerlinkgroup_${i}`}
                 >
-                  <Text as="li" size="sm" className="font-semibold">
-                    {label}
-                  </Text>
+                  <li className={cn("flex flex-row items-center", gaps.sm)}>
+                    {icon && (
+                      <Icon
+                        icon={icon}
+                        color="inherit"
+                        size={smallerSize(size)}
+                        className={cn(adjustedColors[color])}
+                      />
+                    )}
+                    <Text
+                      size={smallerSize(size)}
+                      className={cn("font-semibold", adjustedColors[color])}
+                    >
+                      {label}
+                    </Text>
+                  </li>
                   {links.map(({ label, href }, _i) => (
-                    <Text as="li" size="sm" key={`footergroup_${i}_el_${_i}`}>
+                    <Text
+                      as="li"
+                      size={smallerSize(size)}
+                      color="inherit"
+                      className={cn(adjustedColors[color])}
+                      key={`footergroup_${i}_el_${_i}`}
+                    >
                       <Link href={href}>{label}</Link>
                     </Text>
                   ))}
@@ -71,15 +114,39 @@ export const Footer = forwardRef<HTMLDivElement, FooterProps>(
         </div>
         {(copyright || logo) && (
           <div
-            className={cn("flex flex-col items-center", gaps.md, paddingsY.md)}
+            className={cn(
+              "flex flex-col items-center",
+              gaps[size],
+              paddingsYLarge[size]
+            )}
           >
             <div className={cn("flex justify-center items-center w-full")}>
-              <div className="border-b border-accent-200 dark:border-accent-700 w-full flex-grow" />
+              <div
+                className={cn(
+                  "border-b w-full flex-grow opacity-50",
+                  extendedBorders.inherit,
+                  adjustedColors[color]
+                )}
+              />
               {logo && <div className={cn("flex", paddingsX.xl)}>{logo}</div>}
-              <div className="border-b border-accent-200 dark:border-accent-700 w-full flex-grow" />
+              <div
+                className={cn(
+                  "border-b w-full flex-grow opacity-50",
+                  extendedBorders.inherit,
+                  adjustedColors[color]
+                )}
+              />
             </div>
 
-            <Text size="xs">{copyright}</Text>
+            {copyright && (
+              <Text
+                color="inherit"
+                className={cn(adjustedColors[color])}
+                size="xs"
+              >
+                {copyright}
+              </Text>
+            )}
           </div>
         )}
       </footer>
