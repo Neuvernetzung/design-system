e099fa98c60862ee5e6709698102a139db76f26d
Tim Heerwagen <t.heerwagen@web.de>
2023-11-25 21:58:19 +0100

    Footer Variabler gestalten #1172

---
+++ .changeset/early-grapes-call.md
@@ -0,0 +1,5 @@
+---
+"@neuvernetzung/design-system": minor
+---
+
+Footer Variabler gestalten #1172
--- src/components/common/Footer/footer.stories.tsx
+++ src/components/common/Footer/footer.stories.tsx
@@ -1,21 +1,41 @@
 import { Meta } from "@storybook/react";
+import { IconBeach, IconCheck, IconCross } from "@tabler/icons-react";
+
+import { Heading, Text } from "@/components/ui";
 
 import { Logo } from "../../../../public/Logo";
 import { Icon } from "../../ui/Icon";
 import { Footer } from "./footer";
-import { IconBeach, IconCheck, IconCross } from "@tabler/icons-react";
 
 export default {
   title: "COMMON/Footer",
   component: Footer,
   argTypes: {},
+  parameters: {
+    docs: {
+      source: {
+        type: "code",
+      },
+    },
+  }, // Workaround für https://github.com/storybookjs/storybook/issues/12747#issuecomment-707265001
 } as Meta;
 
 export const Default = ({ ...args }) => (
   <Footer
-    logo={<Icon size="lg" icon={Logo} className="fill-primary-500" />}
-    copyright="Copyright by Neuvernetzung UG"
-    cols={[
+    divider={{
+      logo: <Icon size="lg" icon={Logo} className="fill-primary-500" />,
+    }}
+    main={
+      <div className="flex flex-col gap-2">
+        <Heading level="h3" size="lg">
+          Testfirma
+        </Heading>
+        <Text>Irgendendetwas</Text>
+        <Text size="sm">Social Media</Text>
+      </div>
+    }
+    legalSection="Copyright by Neuvernetzung UG"
+    links={[
       {
         label: "Rechtliches",
         icon: IconBeach,
