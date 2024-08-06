f4c7e75689c23f0baeadfd755fece5277ac6f7bd
Tim Heerwagen <t.heerwagen@web.de>
2024-08-06 09:30:31 +0200

    Prose - Bilder mit floating werden nicht korrekt dargestellt #1529

---
+++ .changeset/cold-seahorses-own.md
@@ -0,0 +1,5 @@
+---
+"@neuvernetzung/design-system": patch
+---
+
+Prose - Bilder mit floating werden nicht korrekt dargestellt #1529
--- src/components/ui/Prose/prose.tsx
+++ src/components/ui/Prose/prose.tsx
@@ -22,11 +22,18 @@ export const Prose = ({ content, className }: ProseProps) => {
 };
 
 export const proseClassName = cn(
+  // Base
   "prose prose-accent dark:prose-invert prose-sm sm:prose-base",
+  // Headings
   "prose-headings:font-heading prose-headings:font-semibold",
+  // Anchor
   "prose-a:after:content-['↗'] prose-a:after:align-middle",
-  "prose-img:rounded-md prose-img:inline-block data-[float=left]:prose-img:mr-4 data-[float=right]:prose-img:ml-4 data-[float=left]:prose-figure:mr-4 data-[float=right]:prose-figure:ml-4",
+  // Image
+  "prose-img:rounded-md prose-img:inline-block",
+  "data-[float=left]:prose-img:mr-4 data-[float=right]:prose-img:ml-4 data-[float=left]:prose-figure:mr-4 data-[float=right]:prose-figure:ml-4",
+  // Video
   "prose-video:rounded-md prose-video:inline-block prose-video:w-full",
+  // Table
   "prose-th:bg-accent-100 dark:prose-th:bg-accent-900 prose-th:border-accent-300 dark:prose-th:border-accent-700",
   "prose-td:border-accent-300 dark:prose-td:border-accent-700"
 );
